55035be4a446c3b52745c408ae620caeb8acf86e
Radosław Olszewski <olszewski.radoslaw15@gmail.com>
2021-10-04 19:23:57 +0200

    change function v1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 {
-const main__button = document.querySelector(".main__button");
-const body = document.querySelector(".body");
-const main__changeColor = document.querySelector(".main__changeColor")
-// let dark = document.querySelector(".dark");
+    const OnChanegeBackgroundClick = () => {
+        body.classList.toggle("dark");
+        const body = document.querySelector(".body");
+        if (body.classList.contains("dark")) {
+            main__changeColor.innerText = "jasny";
+        } else {
+            main__changeColor.innerText = "ciemny";
+        }
 
+        // main__changeColor.innerText = body.classList.contains("dark") ? "jasny" : "ciemny" ;
+    };
 
-const OnChanegeBackgroundClick = () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        main__changeColor.innerText = "jasny";
-    } else {
-        main__changeColor.innerText = "ciemny";
-    }
-
-// main__changeColor.innerText = body.classList.contains("dark") ? "jasny" : "ciemny" ;
-};
-
-main__button.addEventListener("click", OnChanegeBackgroundClick);
+    const init = () => {
+        const main__button = document.querySelector(".main__button");
+        const main__changeColor = document.querySelector(".main__changeColor")
+        // const dark = document.querySelector(".dark");
+        main__button.addEventListener("click", OnChanegeBackgroundClick);
+    };
 
 }
-
